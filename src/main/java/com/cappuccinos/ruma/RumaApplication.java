43645d5a25eaad7f6b2c431193d6ba1633951adc
Lucas Xavier <lucaseoxavier@gmail.com>
2023-09-14 19:27:11 -0300

    [feat] initial commit

---
+++ src/main/java/com/cappuccinos/ruma/RumaApplication.java
@@ -0,0 +1,13 @@
+package com.cappuccinos.ruma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RumaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RumaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cappuccinos/ruma/RumaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cappuccinos.ruma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RumaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
