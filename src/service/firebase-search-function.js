9f2b06ea6df35fb4c918482c3696f8c5878535b2
Sami Nagahama <nagahamasami@gmail.com>
2023-10-26 21:37:51 -0300

    Check if it's empty, convert to lowercase, return empty array for only 1 character

--- src/service/firebase-search-function.js
+++ src/service/firebase-search-function.js
@@ -2,15 +2,21 @@ import { ref, onValue } from 'firebase/database';
 import { db } from './firebase-config';
 
 export function searchProductsByName(searchTerm, callback) {
+    searchTerm = searchTerm.trim().toLowerCase();
+
+    if (searchTerm === '' || searchTerm.length === 1) {
+        callback(null, []);
+        return;
+    }
+
     const productsRef = ref(db, 'products');
     onValue(productsRef, (snapshot) => {
         const allProducts = snapshot.val();
-        const filteredResults = Object.values(allProducts).filter(product => 
-            product.name.includes(searchTerm)
+        const filteredResults = Object.values(allProducts).filter(product =>
+            product.productName.toLowerCase().includes(searchTerm)
         );
         callback(null, filteredResults);
     }, (error) => {
         callback(error);
     });
 }
-
