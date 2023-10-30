a588c7b311ca2f0397fa1c3cebd937ad4e5513a3
Sami Nagahama <nagahamasami@gmail.com>
2023-10-29 21:07:15 -0300

    Addition of log event into navbarProducts

--- src/components/menu/navbarProducts.jsx
+++ src/components/menu/navbarProducts.jsx
@@ -1,20 +1,36 @@
 import React from 'react';
 import './navbarProducts.css'
 
+import { logEvent } from "firebase/analytics";
+import { analytics } from '../../service/firebase-config'; 
+
+function handleLinkClick(e, path, label) {
+  e.preventDefault();
+
+  logEvent(analytics, 'select_content', {
+    content_type: 'nav_link',
+    content_id: label
+  });
+
+  window.location.href = path;
+}
+
+
 const NavbarProducts = () => {
- return (
+  return (
     <nav className="navbarProducts">
       <ul>
-        <li><a href="/produtos">Todos os departamentos</a></li>
-        <li><a href="/produtos">Moda feminina</a></li>
-        <li><a href="/produtos">Moda masculina</a></li>
-        <li><a href="/produtos">Moda infantil</a></li>
-        <li><a href="/produtos">Moda para todes</a></li>
-        <li><a href="/produtos">Acessórios</a></li>
-        <li><a href="/produtos">Calçados</a></li>
+        <li><a href="/produtos" onClick={(e) => handleLinkClick(e, '/produtos', 'Todos os departamentos')}>Todos os departamentos</a></li>
+        <li><a href="/produtos" onClick={(e) => handleLinkClick(e, '/produtos', 'Moda feminina')}>Moda feminina</a></li>
+        <li><a href="/produtos" onClick={(e) => handleLinkClick(e, '/produtos', 'Moda masculina')}>Moda masculina</a></li>
+        <li><a href="/produtos" onClick={(e) => handleLinkClick(e, '/produtos', 'Moda infantil')}>Moda infantil</a></li>
+        <li><a href="/produtos" onClick={(e) => handleLinkClick(e, '/produtos', 'Moda para todes')}>Moda para todes</a></li>
+        <li><a href="/produtos" onClick={(e) => handleLinkClick(e, '/produtos', 'Acessórios')}>Acessórios</a></li>
+        <li><a href="/produtos" onClick={(e) => handleLinkClick(e, '/produtos', 'Calçados')}>Calçados</a></li>
       </ul>
     </nav>
- );
+);
+
 };
 
 export default NavbarProducts;
