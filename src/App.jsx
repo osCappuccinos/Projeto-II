aa99e1741ee9a0ae471792798435c9d41d5a3a01
Sami Nagahama <nagahamasami@gmail.com>
2023-10-30 17:08:43 -0300

    Testing

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,16 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/menu/navbar';
 import NavbarProducts from './components/menu/navbarProducts'
-
+import { fetchTopRatedProducts } from './service/firebaseController'; // adjust path
 
 function App() {
+
+  /*
+  fetchTopRatedProducts(1, (error, data) => {
+    console.log(data)
+  })
+*/ 
+
   return (
     <div className="container">
       <Navbar />
--- src/components/algorithm/TopRatedProducts.jsx
+++ src/components/algorithm/TopRatedProducts.jsx
@@ -9,15 +9,35 @@ function TopRatedProducts() {
   const [products, setProducts] = useState([]);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    fetchTopRatedProducts(5, (err, topRatedProducts) => {
-      if (err) {
-        setError(err);
-      } else {
-        setProducts(topRatedProducts);
-      }
-    });
-  }, []);
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                fetchTopRatedProducts(5, async (err, topRatedProducts) => {
+                    if (err) throw new Error(err);
+
+                    const allProducts = await getAllProductsContent();
+                    
+                    const combinedProducts = topRatedProducts.map(product => {
+                        const matchedProduct = allProducts.find(p => p.id === product.id);
+                        return {
+                            id: product.id,
+                            name: matchedProduct.name,
+                            image: matchedProduct.images[0].file.url,
+                            price: matchedProduct.price,
+                            productName: product.productName // This is assuming you want to pass the productName from Firebase as well
+                        };
+                    });
+
+                    setProducts(combinedProducts);
+                   // console.log(combinedProducts)
+                });
+            } catch (err) {
+                setError(err.message);
+            }
+        };
+
+        fetchProducts();
+    }, []);
 
   if (error) {
     return <div>Error: {error}</div>;
