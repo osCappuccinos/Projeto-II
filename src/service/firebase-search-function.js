74c70321d11f9de05781a1728f80c2b677a69332
Sami Nagahama <nagahamasami@gmail.com>
2023-11-04 20:43:02 -0300

    Fixed search function

--- src/service/firebase-search-function.js
+++ src/service/firebase-search-function.js
@@ -11,12 +11,28 @@ export function searchProductsByName(searchTerm, callback) {
 
     const productsRef = ref(db, 'products');
     onValue(productsRef, (snapshot) => {
-        const allProducts = snapshot.val();
-        const filteredResults = Object.values(allProducts).filter(product =>
-            product.productName.toLowerCase().includes(searchTerm)
-        );
-        callback(null, filteredResults);
+        try {
+            const allProducts = snapshot.val();
+            if (!allProducts) {
+                throw new Error('No products found in database.');
+            }
+
+            const filteredResults = Object.values(allProducts).filter(product => {
+                if (!product || !product.name) { // Alterado de productName para name
+                    console.error('Invalid product or name missing:', product);
+                    return false;
+                }
+                return product.name.toLowerCase().includes(searchTerm); // Alterado de productName para name
+            });
+
+            callback(null, filteredResults);
+        } catch (error) {
+            console.error('Error filtering products:', error);
+            callback(error);
+        }
     }, (error) => {
+        console.error('Error reading from database:', error);
         callback(error);
     });
 }
+
